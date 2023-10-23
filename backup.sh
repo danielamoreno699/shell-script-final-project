7c5b03e9f21ef07e3f8d74d4b1e10c067feff19d
daniela <dani9292@hotmail.com>
2023-10-23 15:26:44 -0500

    completed tasks 13

--- backup.sh
+++ backup.sh
@@ -1,70 +1,67 @@
 #!/bin/bash
 
 # This checks if the number of arguments is correct
-# If the number of arguments is incorrect ( $# != 2) print error message and exit
-if [[ $# != 2 ]]
-then
-  echo "backup.sh target_directory_name destination_directory_name"
-  exit
+# If the number of arguments is incorrect ( $# != 2) print an error message and exit
+if [[ $# != 2 ]]; then
+  echo "Usage: $0 source_directory destination_directory"
+  exit 1
 fi
 
 # This checks if argument 1 and argument 2 are valid directory paths
-if [[ ! -d $1 ]] || [[ ! -d $2 ]]
-then
+if [[ ! -d $1 ]] || [[ ! -d $2 ]]; then
   echo "Invalid directory path provided"
-  exit
+  exit 1
 fi
 
 # [TASK 1]
-targetDirectory= $1
-destinationDirectory= $2
+targetDirectory="$1"
+destinationDirectory="$2"
 
 # [TASK 2]
-echo "command argument line $1"
-echo "command argument line $2"
+echo "Source directory: $1"
+echo "Destination directory: $2"
 
 # [TASK 3]
-currentTS=`(date +%s)`
+currentTS=$(date +%s)
 
 # [TASK 4]
-backupFileName="backup-[$currentTS].tar.gz"
+backupFileName="backup-${currentTS}.tar.gz"
 
 # We're going to:
-  # 1: Go into the target directory
-  # 2: Create the backup file
-  # 3: Move the backup file to the destination directory
+# 1: Go into the target directory
+# 2: Create the backup file
+# 3: Move the backup file to the destination directory
 
 # To make things easier, we will define some useful variables...
 
 # [TASK 5]
-origAbsPath=$(/mnt/c/Users/us/Documents/projects1/shell-script-final-project/current_directory)
+origAbsPath=$(pwd)
 
 # [TASK 6]
-cd # <-
-destDirAbsPath=$(/mnt/c/Users/us/Documents/projects1/shell-script-final-project/destination_directory)
+cd "$destinationDirectory"
+destDirAbsPath=$(pwd)
 
 # [TASK 7]
-cd  current_directory
-cd  destination_directory
+cd "$origAbsPath"
 
 # [TASK 8]
-yesterdayTS= (($currentTs - 24 * 60 * 60))
+yesterdayTS=$((currentTS - 24 * 60 * 60))
 
 declare -a toBackup
 
-for file in $(origAbsPath) # [TASK 9]
+for file in $(ls "$origAbsPath") # [TASK 9]
 do
-  
   # [TASK 10]
-  if ((`date -r $file +%s` > $yesterdayTS))
-  then
+  if [[ $(date -r "$file" +%s) -gt $yesterdayTS ]]; then
     # [TASK 11]
-    toBackup+=($file)
+    toBackup+=("$file")
   fi
 done
 
 # [TASK 12]
+tar -czvf "$backupFileName" "${toBackup[@]}"
 
 # [TASK 13]
+echo "Backup file $backupFileName created in $origAbsPath"
 
 # Congratulations! You completed the final project for this course!
